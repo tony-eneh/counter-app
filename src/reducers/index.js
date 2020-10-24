981d8edcdfc661070703a12fc7b1fe14a0adefc5
Tony Eneh <aeneh90@gmail.com>
2020-10-24 13:48:42 +0100

    Feat: Get reducer working mbok

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,21 @@
 export const reducer = (state, action) => {
-  return { ...state };
+  switch (action.type) {
+    case "SET_ACTIVE_SESSION":
+      return { ...state, activeSession: action.payload.activeSession };
+    case "CHANGE_TIME":
+      // depending on the direction of the change action
+      // we will either be adding or subtracting a 1 from the activeSession variable
+      const delta = action.payload.direction === "INCREASE_COUNTER" ? 1 : -1;
+
+      // convert to small letters to match the format of the object properties
+      const activeSession = state.activeSession.toLowerCase();
+      const activeSessionValue = state[activeSession];
+      const newValue = activeSessionValue + delta;
+
+      // I am using ES6 computed value syntax [] to assign the new value to the correct active session
+      return { ...state, [activeSession]: newValue };
+
+    default:
+      return { ...state };
+  }
 };
